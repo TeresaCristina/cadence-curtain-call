8d5c75a343298b8fe9c7b383f57c0f3d942b7a63
TeresaComputer <tcpaixao-costa@myseneca.ca>
2021-09-17 22:56:39 -0400

    fix heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/paper-kit-2-angular/dist'));
+app.use(express.static(__dirname + '/dist/paper-kit-2-angular'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/paper-kit-2-angular/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/paper-kit-2-angular/index.html'));
 });
     
 
