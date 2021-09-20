e9ee4a081a1c45b5cdf85c37335ed5c0135ba4e5
TeresaComputer <tcpaixao-costa@myseneca.ca>
2021-09-20 18:35:33 -0400

    timeline

---
+++ src/app/tabs/timeline/card-timeline/card-timeline.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardTimelineComponent } from './card-timeline.component';
+
+describe('CardTimelineComponent', () => {
+  let component: CardTimelineComponent;
+  let fixture: ComponentFixture<CardTimelineComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardTimelineComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardTimelineComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
