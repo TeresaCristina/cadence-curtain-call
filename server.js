9dd2b2e8f96e596a154d42cdb55b52b3f026a8ab
TeresaComputer <tcpaixao-costa@myseneca.ca>
2021-09-20 19:23:17 -0400

    server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+
+
+mongoose.connect(`mongodb+srv://viola:cadencetheatre@cadence.ycthy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+.then(() => {
+  console.log('Connected to database');
+  port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+  });
+});
 
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist/paper-kit-2-angular'));
@@ -10,6 +21,3 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/paper-kit-2-angular/index.html'));
 });
     
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
